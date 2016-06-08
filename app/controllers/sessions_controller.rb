9dd9a38cec7091353be9a848916a6dfb2274fb07
Hines Kao <hineskao@gmail.com>
2016-06-07 17:09:50 -0700

    Login now goes to the correct user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       flash[:success] = "Success"
-      redirect_to root_path
+      redirect_to user_path(@user.id)
     else
       flash[:failure] = "hello"
       redirect_to new_session_path
